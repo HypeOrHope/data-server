c7e86336f45c7648a4b1621ac83e1071e1573d0e
André Kovac <me@andrekovac.com>
2019-01-03 16:43:23 +0100

    Add basic article type

--- src/HoH/Types.hs
+++ src/HoH/Types.hs
@@ -1,2 +1,27 @@
 module HoH.Types where
 
+-- import Date.Dates (DateTime)
+
+data ArticleUrl = ArticleUrl {
+  id :: String,
+  url :: String,
+}
+
+type ArticleUrls = [ArticleUrl]
+
+data ArticleHtml = ArticleHtml {
+  id :: String,
+  html :: String,
+}
+
+{-
+  A guardian article
+-}
+data Article = Article {
+  id        :: String,
+  title     :: String,
+  text      :: String,
+  date      :: String,
+  section   :: String,
+  url       :: String
+} deriving (Show, Read)
