d9e7886d013e45fd545e59c8f4979d8beae54d94
André Kovac <me@andrekovac.com>
2019-01-03 16:57:12 +0100

    Improve syntax of data types

--- src/HoH/Types.hs
+++ src/HoH/Types.hs
@@ -2,26 +2,27 @@ module HoH.Types where
 
 -- import Date.Dates (DateTime)
 
-data ArticleUrl = ArticleUrl {
-  id :: String,
-  url :: String,
-}
+data ArticleUrl = ArticleUrl
+  {
+    , id :: String
+    , url :: String
+  }
 
 type ArticleUrls = [ArticleUrl]
 
-data ArticleHtml = ArticleHtml {
-  id :: String,
-  html :: String,
-}
+data ArticleHtml = ArticleHtml
+  { id :: String
+  , html :: String
+  }
 
 {-
   A guardian article
 -}
-data Article = Article {
-  id        :: String,
-  title     :: String,
-  text      :: String,
-  date      :: String,
-  section   :: String,
-  url       :: String
+data Article = Article
+{ id        :: String
+  , title     :: String
+  , text      :: String
+  , date      :: String
+  , section   :: String
+  , url       :: String
 } deriving (Show, Read)
