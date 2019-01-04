36f33f899488e7723692b3ccbd40359c669e1d46
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-01-04 11:45:27 +0100

    Add proper filenaming for downloadHtml

--- src/HoH/LoadArticles.hs
+++ src/HoH/LoadArticles.hs
@@ -7,20 +7,23 @@ import Network.HTTP.Conduit (simpleHttp)
 import qualified Data.ByteString.Lazy.Char8 as BSL8
 import qualified Data.ByteString.Lazy as BL
 
+
+-- type alias
+type Url = String
+
 -- the URL we're going to search
+myurl :: Url
 myurl = "https://www.theguardian.com/games/2018/dec/11/the-11-best-games-on-playstation-vr"
 
--- Alternative:
--- output = L.putStrLn . L.take 500 =<< simpleHttp url
+makePath :: Url -> FilePath
+makePath url = ("data/html/" ++ [ if c == '/' then '_' else c | c <- drop 8 url ] ++ ".html")
 
-
-downloadHtml :: String -> IO ()
+downloadHtml :: Url -> IO ()
 downloadHtml url = do
     bytes <- simpleHttp url
-    -- L.putStrLn (L.take 500 bytes)
-    BSL8.putStrLn (bytes)
     createDirectoryIfMissing True "data/html"
-    handle <- openFile "data/html/playstation.html" WriteMode
+    let path = makePath url
+    handle <- openFile path WriteMode
     BL.hPut handle bytes
     hClose handle
 
