b1d5e6e0f90cf31920aba71516946e58ebfacf7d
Jannik Buhr <jannik.m.buhr@gmail.com>
2019-01-04 09:31:51 +0100

    Add download function

--- src/HoH/LoadArticles.hs
+++ src/HoH/LoadArticles.hs
@@ -2,22 +2,27 @@ module HoH.LoadArticles where
 
 import Text.Read (readMaybe)
 import System.IO
+import System.Directory
 import Network.HTTP.Conduit (simpleHttp)
-import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Lazy.Char8 as BSL8
+import qualified Data.ByteString.Lazy as BL
 
 -- the URL we're going to search
-url = "https://www.theguardian.com/games/2018/dec/11/the-11-best-games-on-playstation-vr"
+myurl = "https://www.theguardian.com/games/2018/dec/11/the-11-best-games-on-playstation-vr"
 
 -- Alternative:
 -- output = L.putStrLn . L.take 500 =<< simpleHttp url
 
 
-
-output = do
+downloadHtml :: String -> IO ()
+downloadHtml url = do
     bytes <- simpleHttp url
     -- L.putStrLn (L.take 500 bytes)
-    L.putStrLn (bytes)
-    handle <- openFile "playstation.html" WriteMode
-    hPutStrLn handle $ L.unpack bytes
+    BSL8.putStrLn (bytes)
+    createDirectoryIfMissing True "data/html"
+    handle <- openFile "data/html/playstation.html" WriteMode
+    BL.hPut handle bytes
     hClose handle
 
+parseHtml = do
+    undefined
