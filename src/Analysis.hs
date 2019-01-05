c82b4b5517339ba670b2122a8f94f4ee57838952
Niklas Hambüchen <mail@nh2.me>
2019-01-05 16:03:57 +0100

    Better deduplication

--- src/Analysis.hs
+++ src/Analysis.hs
@@ -4,9 +4,11 @@
 module Analysis where
 
 import           Data.ByteString (ByteString)
-import           Data.Foldable (for_)
 import qualified Data.ByteString as BS
+-- import           Data.Containers.ListUtils (nubOrdOn)
+import           Data.Foldable (for_)
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import           Say (say)
@@ -24,8 +26,21 @@ showText :: (Show a) => a -> T.Text
 showText = T.pack . show
 
 
-deduplicate :: (Ord a) => [a] -> [a]
-deduplicate = Set.toList . Set.fromList
+ordNubBy :: (Ord b) => (a -> b) -> (a -> a -> Bool) -> [a] -> [a]
+ordNubBy p f l = go Map.empty l
+  where
+    go _ []     = []
+    go m (x:xs) = let b = p x in case b `Map.lookup` m of
+                    Nothing     -> x : go (Map.insert b [x] m) xs
+                    Just bucket
+                      | elem_by f x bucket -> go m xs
+                      | otherwise -> x : go (Map.insert b (x:bucket) m) xs
+
+
+-- From the Data.List source code.
+elem_by :: (a -> a -> Bool) -> a -> [a] -> Bool
+elem_by _  _ []     = False
+elem_by eq y (x:xs) = y `eq` x || elem_by eq y xs
 
 
 runAnalysis :: IO ()
@@ -37,7 +52,7 @@ runAnalysis = do
 
   -- First download all API results to build list of all articles
   -- to download before downloading any contents.
-  allArticlesWithKeywords <- fmap deduplicate . fmap concat $ pooledForConcurrentlyN 4 keywords $ \keyword -> do
+  allArticlesWithKeywords <- fmap (ordNubBy fst (==)) . fmap concat $ pooledForConcurrentlyN 4 keywords $ \keyword -> do
     say $ "Downloading articles for keyword: " <> T.pack keyword
     articles <- getArticlesByKeyword keyword
     return [ (a, keyword) | a <- articles ]
