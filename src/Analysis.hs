94b79c6d3c624530ac5bd57a65453357150e8e0f
Niklas Hambüchen <mail@nh2.me>
2019-01-05 11:02:55 +0100

    Build list of articles to download ahead of time

--- src/Analysis.hs
+++ src/Analysis.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import           Say (say)
 import           System.Directory (doesFileExist)
-import           UnliftIO.Async (pooledForConcurrentlyN_)
+import           UnliftIO.Async (pooledForConcurrentlyN, pooledForConcurrentlyN_)
 
 import GuardianApi (getArticlesByKeyword)
 import LoadArticles (makePath, downloadHtml)
@@ -24,20 +24,24 @@ runAnalysis = do
         , "artificial intelligence"
         ]
 
-  pooledForConcurrentlyN_ 4 keywords $ \keywords -> do
-    articleList <- getArticlesByKeyword keywords
+  -- First download all API results to build list of all articles
+  -- to download before downloading any contents.
+  allArticles <- fmap concat $ pooledForConcurrentlyN 4 keywords $ \keyword -> do
+    say $ "Downloading articles for keyword: " <> T.pack keyword
+    getArticlesByKeyword keyword
 
-    pooledForConcurrentlyN_ 100 articleList $ \ApiArticle{ aurl } -> do
+  -- Now, download all article contents.
+  pooledForConcurrentlyN_ 100 allArticles $ \ApiArticle{ aurl } -> do
 
-      let path = makePath aurl
-      exists <- doesFileExist path
-      if exists
-        then say $ "Using from cache: " <> T.pack aurl
-        else do
-          say $ "Fetching: " <> T.pack aurl
-          downloadHtml aurl
+    let path = makePath aurl
+    exists <- doesFileExist path
+    if exists
+      then say $ "Using from cache: " <> T.pack aurl
+      else do
+        say $ "Fetching: " <> T.pack aurl
+        downloadHtml aurl
 
-      -- page <- BS.readFile path
-      -- let texts = runScraper page
+    -- page <- BS.readFile path
+    -- let texts = runScraper page
 
-      -- print texts
+    -- print texts
